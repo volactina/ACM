20375dd58ebcb44668310de662e182a23703e28f
volactina <10165101137@stu.ecnu.edu.cn>
2020-03-18 10:24:25 +0800

    Add files via upload

---
+++ leetcode/836/Solution.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+        if(rec2[1]>=rec1[3]||rec2[3]<=rec1[1]) return false;
+        if(rec2[0]>=rec1[2]||rec2[2]<=rec1[0]) return false;
+        return true;
+    }
+}
